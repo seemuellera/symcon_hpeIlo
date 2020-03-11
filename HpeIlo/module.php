959695124cb223e42533cb640d5b55f3429383c3
Andreas Seemueller <andreas.seemueller@gmail.com>
2020-03-11 09:46:39 +0100

    Modified wrong file

--- HpeIlo/module.php
+++ HpeIlo/module.php
@@ -1,7 +1,7 @@
 <?php
 
 // Klassendefinition
-class MetaSwitch extends IPSModule {
+class HpeIlo extends IPSModule {
  
 	// Der Konstruktor des Moduls
 	// Überschreibt den Standard Kontruktor von IPS
@@ -20,19 +20,22 @@ class MetaSwitch extends IPSModule {
 		parent::Create();
 
 		// Properties
-		$this->RegisterPropertyString("Sender","MetaSwitch");
+		$this->RegisterPropertyString("Sender","HpeIlo");
 		$this->RegisterPropertyInteger("RefreshInterval",0);
+		$this->RegisterPropertyString("hostname","");
+		$this->RegisterPropertyString("username","");
+		$this->RegisterPropertyString("password","");
 
 		// Variables
-		$this->RegisterVariableBoolean("Status","Status","~Switch");
-		$this->RegisterVariableString("Devices","Devices");
-		$this->RegisterVariableString("DeviceTriggers","DeviceTriggers");
+		// $this->RegisterVariableBoolean("Status","Status","~Switch");
+		$this->RegisterVariableString("SystemHealth","System Health");
+		
 
 		// Default Actions
 		$this->EnableAction("Status");
 
 		// Timer
-		$this->RegisterTimer("RefreshInformation", 0 , 'METASWITCH_RefreshInformation($_IPS[\'TARGET\']);');
+		$this->RegisterTimer("RefreshInformation", 0 , 'HPEILO_RefreshInformation($_IPS[\'TARGET\']);');
 
     }
 
@@ -69,9 +72,11 @@ class MetaSwitch extends IPSModule {
 		
 
 		// Add the buttons for the test center
-		$form['actions'][] = Array("type" => "Button", "label" => "Refresh Overall Status", "onClick" => 'METASWITCH_RefreshInformation($id);');
+		$form['actions'][] = Array("type" => "Button", "label" => "Refresh Overall Status", "onClick" => 'HPEILO_RefreshInformation($id);');
+		/*
 		$form['actions'][] = Array("type" => "Button", "label" => "Switch On", "onClick" => 'METASWITCH_SwitchOn($id);');
 		$form['actions'][] = Array("type" => "Button", "label" => "Switch Off", "onClick" => 'METASWITCH_SwitchOff($id);');
+		*/
 
 		// Return the completed form
 		return json_encode($form);
@@ -80,170 +85,8 @@ class MetaSwitch extends IPSModule {
 
 	public function RefreshInformation() {
 
-		// Let us assume that alls is off
-		$status = false;
-	
-		$allDevices = $this->GetDevices();
+		IPS_LogMessage($_IPS['SELF'],"HPEILO - Refresh in progress");
 
-		foreach ($allDevices as $currentDevice) {
-		
-			// If one device is on we set the status to on
-			if (GetValue($currentDevice) ) {
-			
-				$status = true;	
-				// As we have found a single switch on device we can stop processing the loop
-				break;
-			}
-		}
-
-		SetValue($this->GetIDForIdent("Status"), $status);
-		
-		$this->UpdateDeviceTriggers();
-
-	}
-
-	public function SwitchOn() {
-	
-		$allDevices = $this->GetDevices();
-
-		foreach ($allDevices as $currentDevice) {
-		
-			$currentDeviceDetails = IPS_GetVariable($currentDevice);
-			$parentId = $currentDeviceDetails['VariableAction'];
-
-			if (! IPS_InstanceExists($parentId) ) {
-			
-				IPS_LogMessage($_IPS['SELF'],"METASWITCH - SwitchOn not possible for device $currentDevice - parent instance was not found");
-				// Now we skip this device
-				continue;
-			}
-
-			// Now we need to find out which device type we have to deal with
-			$parentDetails = IPS_GetInstance($parentId);
-			$parentModuleName = $parentDetails['ModuleInfo']['ModuleName'];
-
-			if (preg_match('/Z-Wave/', $parentModuleName) ) {
-			
-				ZW_SwitchMode($parentId, true);
-				continue;
-			}
-
-			if (preg_match('/HUELight/', $parentModuleName) ) {
-			
-				HUE_SetState($parentId, true);
-				continue;
-			}
-
-			if (preg_match('/MetaSwitch/', $parentModuleName) ) {
-			
-				METASWITCH_SwitchOn($parentId);
-			}
-			
-			IPS_LogMessage($_IPS['SELF'],"METASWITCH - SwitchOn not possible for device $currentDevice - could not identify instance type");
-		}
-	}
-
-	public function SwitchOff() {
-	
-		$allDevices = $this->GetDevices();
-
-		foreach ($allDevices as $currentDevice) {
-		
-			$currentDeviceDetails = IPS_GetVariable($currentDevice);
-			$parentId = $currentDeviceDetails['VariableAction'];
-
-			if (! IPS_InstanceExists($parentId) ) {
-			
-				IPS_LogMessage($_IPS['SELF'],"METASWITCH - SwitchOff not possible for device $currentDevice - parent instance was not found");
-				// Now we skip this device
-				continue;
-			}
-
-			// Now we need to find out which device type we have to deal with
-			$parentDetails = IPS_GetInstance($parentId);
-			$parentModuleName = $parentDetails['ModuleInfo']['ModuleName'];
-
-			if (preg_match('/Z-Wave/', $parentModuleName) ) {
-			
-				ZW_SwitchMode($parentId, false);
-				continue;
-			}
-
-			if (preg_match('/HUELight/', $parentModuleName) ) {
-			
-				HUE_SetState($parentId, false);
-				continue;
-			}
-
-			if (preg_match('/MetaSwitch/', $parentModuleName) ) {
-				                        
-				METASWITCH_SwitchOff($parentId);
-			}
-			
-			IPS_LogMessage($_IPS['SELF'],"METASWITCH - SwitchOff not possible for device $currentDevice - could not identify instance type");
-		}
-
-	}
-	
-	protected function UpdateDeviceTriggers() {
-		
-		$deviceTriggersId = $this->GetIDForIdent("DeviceTriggers");
-		$allDeviceTriggers = IPS_GetChildrenIDs($deviceTriggersId);
-		$allDevices = $this->GetDevices();
-		
-		$allDeviceTriggerNames = Array();
-		
-		$instanceId = IPS_GetParent($deviceTriggersId);
-		
-		foreach ($allDeviceTriggers as $currentDeviceTrigger) {
-			
-			$currentDeviceTriggerName = IPS_GetName($currentDeviceTrigger);
-			$allDeviceTriggerNames[] = $currentDeviceTriggerName;
-		}
-		
-		// Create missing devices
-		foreach ($allDevices as $currentDevice) {
-			
-			if (! in_array($currentDevice, $allDeviceTriggerNames) ) {
-				
-				IPS_LogMessage($_IPS['SELF'],"METASWITCH - Creating missing event trigger for device $currentDevice");
-				
-				$currentEventId = IPS_CreateEvent(0);
-				IPS_SetParent($currentEventId, $deviceTriggersId);
-				IPS_SetName($currentEventId, $currentDevice);
-				IPS_SetEventTrigger($currentEventId, 1, $currentDevice);
-				IPS_SetEventScript($currentEventId, 'METASWITCH_RefreshInformation(' . $instanceId . ');');
-				IPS_SetEventActive($currentEventId, true);
-			}
-		}
-		
-		// Delete surplus devices
-		foreach ($allDeviceTriggers as $currentDeviceTrigger) {
-			
-			$currentDeviceTriggerName = IPS_GetName($currentDeviceTrigger);
-			
-			if (! in_array($currentDeviceTriggerName, $allDevices) ) {
-				
-				IPS_LogMessage($_IPS['SELF'],"METASWITCH - Removing surplus device trigger $currentDeviceTrigger");
-				
-				IPS_DeleteEvent($currentDeviceTrigger);
-			}
-		}
-	}
-
-	protected function GetDevices() {
-	
-		$allLinks = IPS_GetChildrenIDs($this->GetIDForIdent("Devices"));
-
-		$allDevices = Array();
-
-		foreach ($allLinks as $currentLink) {
-		
-			$currentLinkDetails = IPS_GetLink($currentLink);
-			$allDevices[] = $currentLinkDetails['TargetID'];
-		}
-
-		return $allDevices;
 	}
 
 	public function RequestAction($Ident, $Value) {
@@ -251,6 +94,7 @@ class MetaSwitch extends IPSModule {
 	
 		switch ($Ident) {
 		
+			/*
 			case "Status":
 				// Default Action for Status Variable
 				if ($Value) {
@@ -267,8 +111,9 @@ class MetaSwitch extends IPSModule {
 				break;
 			default:
 				throw new Exception("Invalid Ident");
+			*/
 		}
 	}
 
-    }
+}
 ?>

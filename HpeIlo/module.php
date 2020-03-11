476f80af4294bfad82e4e88311e562583fc2fe2b
Andreas Seemueller <andreas.seemueller@gmail.com>
2020-03-11 10:30:23 +0100

    Added temperature data

--- HpeIlo/module.php
+++ HpeIlo/module.php
@@ -29,6 +29,13 @@ class HpeIlo extends IPSModule {
 		// Variables
 		// $this->RegisterVariableBoolean("Status","Status","~Switch");
 		$this->RegisterVariableString("SystemHealth","System Health");
+		$this->RegisterVariableFloat("TemperatureInlet","Temperature - Inlet","~Temperature");
+		$this->RegisterVariableFloat("TemperatureCpu1","Temperature - CPU 1","~Temperature");
+		$this->RegisterVariableFloat("TemperatureCpu2","Temperature - CPU 2","~Temperature");
+		$this->RegisterVariableFloat("TemperatureGpu","Temperature - GPU","~Temperature");
+		$this->RegisterVariableFloat("TemperaturePs1","Temperature - Power Supply 1","~Temperature");
+		$this->RegisterVariableFloat("TemperaturePs2","Temperature - Power Supply 2","~Temperature");
+		$this->RegisterVariableFloat("TemperatureSystemBoard","Temperature - System Board","~Temperature");
 		
 
 		// Default Actions
@@ -166,5 +173,41 @@ class HpeIlo extends IPSModule {
 		SetValue($this->GetIDForIdent("SystemHealth") , $resultObject->Status->Health);
 	}
 
+	protected function updateThermalData() {
+		
+		$url = "https://" . $this->ReadPropertyString("hostname") . "/rest/v1/Chassis/1/Thermal";
+		$result = $this->CallAPI("GET",$url);
+
+		$resultObject = json_decode($result);
+		//print_r($resultChassisObject);
+		SetValue($this->GetIDForIdent("SystemHealth") , $resultObject->Status->Health);
+		
+		foreach ($resultThermalObject->Temperatures as $currentSensor) {
+
+			switch ($currentSensor->Name) {
+				case "01-Inlet Ambient":
+					SetValue($this->GetIDForIdent("TemperatureInlet"), $currentSensor->CurrentReading);
+					break;
+				case "02-CPU 1":
+					SetValue($this->GetIDForIdent("TemperatureCpu1"), $currentSensor->CurrentReading);
+					break;
+				case "03-CPU 2":
+					SetValue($this->GetIDForIdent("TemperatureCpu2"), $currentSensor->CurrentReading);
+					break;
+				case "19-PS 1 Internal":
+					SetValue($this->GetIDForIdent("TemperaturePs1"), $currentSensor->CurrentReading);
+					break;
+				case "20-PS 2 Internal":
+					SetValue($this->GetIDForIdent("TemperaturePs2"), $currentSensor->CurrentReading);
+					break;
+				case "25-PCI 5 GPU":
+					SetValue($this->GetIDForIdent("TemperatureGpu"), $currentSensor->CurrentReading);
+					break;
+				case "10-Chipset":
+					SetValue($this->GetIDForIdent("TemperatureSystemBoard"), $currentSensor->CurrentReading);
+					break;
+			}
+		}
+	}
 }
 ?>
